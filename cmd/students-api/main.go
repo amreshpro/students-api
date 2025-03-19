c1dae880cdf2f6ae27138d82ac0f593a67eb0886
Amresh Maurya <amresh.terminal@gmail.com>
2025-03-19 22:59:21 +0530

    basic blue print

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/amreshpro/students-api/internal/config"
+)
+
 func main() {
-	println("Hello Go")
+
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		// w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("welcome to students api"))
+	})
+	// setup server
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	println("Server started")
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatal("failed to start server")
+	}
 }
